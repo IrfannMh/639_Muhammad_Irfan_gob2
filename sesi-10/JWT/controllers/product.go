bd79efbaba4d73f904c30333ea8a674909f15d24
IrfannMh <irfann.mmm4@gmail.com>
2022-10-10 21:54:56 +0700

    update sesi-10

---
+++ sesi-10/JWT/controllers/product.go
@@ -0,0 +1,42 @@
+package controllers
+
+import (
+	"JWT/database"
+	"JWT/helpers"
+	"JWT/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	appJSON = "application/json"
+)
+
+func CreateProduct(c *gin.Context) {
+	db := database.GetDB()
+	userData := c.MustGet("userData").(jwt.MapClaims)
+	contentType := helpers.GetContentType(c)
+
+	Product := models.Product{}
+	userID := uint(userData["id"].(float64))
+
+	if contentType == appJSON {
+		c.ShouldBindJSON(&Product)
+	} else {
+		c.ShouldBind(&Product)
+	}
+
+	Product.UserID = userID
+
+	err := db.Debug().Create(&Product).Error
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+			"error":   "Bad request",
+		})
+		return
+	}
+	c.JSON(http.StatusCreated, Product)
+}
---
+++ sesi-10/JWT/controllers/userControllers.go
@@ -0,0 +1,83 @@
+package controllers
+
+import (
+	"JWT/database"
+	"JWT/helpers"
+	"JWT/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	appJSON = "application/json"
+)
+
+func UserRegister(c *gin.Context) {
+	db := database.GetDB()
+	contentType := helpers.GetContentType(c)
+	_, _ = db, contentType
+	User := models.User{}
+
+	if contentType == appJSON {
+		c.ShouldBindJSON(&User)
+	} else {
+		c.ShouldBind(&User)
+	}
+
+	err := db.Debug().Create(&User).Error
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+			"error":   "Bad request",
+		})
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{
+		"id":        User.ID,
+		"email":     User.Email,
+		"full_name": User.FullName,
+	})
+}
+
+func UserLogin(c *gin.Context) {
+	db := database.GetDB()
+	contentType := helpers.GetContentType(c)
+	_, _ = db, contentType
+	User := models.User{}
+	password := ""
+
+	if contentType == appJSON {
+		c.ShouldBindJSON(&User)
+	} else {
+		c.ShouldBind(&User)
+	}
+
+	password = User.Password
+
+	err := db.Debug().Where("email = ?", User.Email).Take(&User).Error
+
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"message": "invalid email",
+			"error":   "Unauthorized",
+		})
+		return
+	}
+
+	comparePass := helpers.ComparePass([]byte(User.Password), []byte(password))
+	if !comparePass {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"message": "password salah",
+			"error":   "Unauthorized",
+		})
+		return
+	}
+
+	token := helpers.GenerateToken(User.ID, User.Email)
+
+	c.JSON(http.StatusOK, gin.H{
+		"token": token,
+	})
+}
